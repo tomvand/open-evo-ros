54b089352a9b3945917c0b2b896464942f51733c
Tom van Dijk <tomvand@users.noreply.github.com>
2018-04-09 14:59:52 +0000

    Get rates and publish odometry message

--- src/openevo_node.cpp
+++ src/openevo_node.cpp
@@ -9,6 +9,7 @@
 #include <image_geometry/pinhole_camera_model.h>
 
 #include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
 
 #include <opencv2/highgui/highgui.hpp>
 
@@ -21,6 +22,7 @@ openevo::EVO evo;
 image_geometry::PinholeCameraModel model;
 
 tf::TransformBroadcaster *tf_bcaster;
+ros::Publisher odom_pub;
 
 template<class T>
 void rvec_to_quat(cv::Mat &rvec, T &quat) {
@@ -32,6 +34,13 @@ void rvec_to_quat(cv::Mat &rvec, T &quat) {
 	quat.x = axis.at<double>(0, 0) * std::sin(angle / 2);
 	quat.y = axis.at<double>(1, 0) * std::sin(angle / 2);
 	quat.z = axis.at<double>(2, 0) * std::sin(angle / 2);
+}
+
+template<class T>
+void vec_to_xyz(cv::Mat &vec, T &xyz) {
+	xyz.x = vec.at<double>(0, 0);
+	xyz.y = vec.at<double>(1, 0);
+	xyz.z = vec.at<double>(2, 0);
 }
 
 void on_image(
@@ -47,19 +56,31 @@ void on_image(
 	cv::Mat intr;
 	static_cast<cv::Mat>(model.projectionMatrix()).copyTo(intr); // Note: intrinsic matrix is 0 on SLAMDunk...
 	intr = intr(cv::Range(0, 3), cv::Range(0, 3));
-	evo.updateImageDepth(colorbr->image, depthbr->image, intr);
+	evo.updateImageDepth(colorbr->image, depthbr->image, intr,
+			caminfo->header.stamp.toSec());
 	// Broadcast pose
 	cv::Mat rvec, tvec;
 	evo.getPose(rvec, tvec);
 	geometry_msgs::TransformStamped tf_msg;
-	tf_msg.header.stamp = ros::Time::now(); // TODO replace with image timestamp
+	tf_msg.header.stamp = caminfo->header.stamp;
 	tf_msg.header.frame_id = "odom";
 	tf_msg.child_frame_id = "evo";
-	tf_msg.transform.translation.x = tvec.at<double>(0, 0);
-	tf_msg.transform.translation.y = tvec.at<double>(1, 0);
-	tf_msg.transform.translation.z = tvec.at<double>(2, 0);
+	vec_to_xyz(tvec, tf_msg.transform.translation);
 	rvec_to_quat(rvec, tf_msg.transform.rotation);
 	tf_bcaster->sendTransform(tf_msg); // tf_bcaster should always exist
+	// Broadcast odom
+	nav_msgs::Odometry odom_msg;
+	odom_msg.header.stamp = caminfo->header.stamp;
+	odom_msg.header.frame_id = "odom";
+	odom_msg.child_frame_id = "evo";
+	vec_to_xyz(tvec, odom_msg.pose.pose.position);
+	rvec_to_quat(rvec, odom_msg.pose.pose.orientation);
+	cv::Mat vel, rates;
+	evo.getRates(vel, rates);
+	vec_to_xyz(vel, odom_msg.twist.twist.linear);
+	vec_to_xyz(rates, odom_msg.twist.twist.angular);
+	odom_pub.publish(odom_msg);
+
 
 	char key = cv::waitKey(1);
 	if(key == 27 || key == 'q') {
@@ -73,6 +94,9 @@ int main(int argc, char **argv) {
 	// Initialize ROS
 	ros::init(argc, argv, "percevite");
 	ros::NodeHandle nh;
+
+	// Advertise tf and odom
+	odom_pub = nh.advertise<nav_msgs::Odometry>("odom", 50);
 	tf_bcaster = new tf::TransformBroadcaster();
 
 	// Subscribe to image topics
